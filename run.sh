815a47b65d7001bb41d6eacbed128d468ee63420
joaoalegria <joao.p@ua.pt>
2018-12-26 16:29:56 +0000

    canal de comunicacao entre cliente e repositorio encriptado

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-xterm -hold -e "python3 /home/joao/Desktop/SIO/Project/sioManager.py" &
-xterm -hold -e "python3 /home/joao/Desktop/SIO/Project/sioRepository.py" &
+xterm -hold -e "python3 sioManager.py" &
+xterm -hold -e "python3 sioRepository.py" &
 sleep 1
-xterm -hold -e "python3 /home/joao/Desktop/SIO/Project/sioClient.py" &
+xterm -hold -e "python3 sioClient.py" &
--- sioRepository.py
+++ sioRepository.py
@@ -1,14 +1,85 @@
 import asyncio
 import websockets
 from Repository import Repository
+import json
+import os
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 repo = Repository()
 
+def decryptMsg(request, private_key):
+    requestList = request.split(b"PROJ_SIO_2018")
+
+    symmetric_key = private_key.decrypt(
+        requestList[0],
+        padding.PKCS1v15()
+    )
+
+    symmetric_iv = private_key.decrypt(
+        requestList[1],
+        padding.PKCS1v15()
+    )
+
+    cipher = Cipher(algorithms.AES(symmetric_key), modes.OFB(symmetric_iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    message = decryptor.update(requestList[2]) + decryptor.finalize()
+
+    jsonData = json.loads(message.decode("utf-8"))
+    client_public_key = jsonData["key"]
+    del jsonData["key"]
+
+    return symmetric_key, symmetric_iv, client_public_key, json.dumps(jsonData)
+
+
+def encryptMsg(response, public_key):
+    message = str.encode(response)
+
+    symmetric_key = os.urandom(32)
+    symmetric_iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(symmetric_key), modes.OFB(symmetric_iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ct = encryptor.update(message) + encryptor.finalize()
+
+
+    key_cyphered = public_key.encrypt(
+        symmetric_key,
+        padding.PKCS1v15()
+    )
+
+    iv_cyphered = public_key.encrypt(
+        symmetric_iv,
+        padding.PKCS1v15()
+    )
+
+    out= key_cyphered+ b"PROJ_SIO_2018"+ iv_cyphered+ b"PROJ_SIO_2018"+ ct
+
+    return out
+
+
+
 async def sioRepository(websocket, path):
-    while True:
-        request = await websocket.recv()
-        print(request)
-        #await websocket.send(await repo.process(request))
+    with open("server_private_key.pem", "rb") as server_private_key_file:
+        server_private_key = serialization.load_pem_private_key(server_private_key_file.read(), password=b"SIO_85048_85122", backend=default_backend())
+
+        while True:
+            request = await websocket.recv()
+            
+            symmetric_key, symmetric_iv, client_public_key_str, data = decryptMsg(request, server_private_key)
+
+            response = await repo.process(data)
+
+            client_public_key = serialization.load_pem_public_key(
+                str.encode(client_public_key_str),
+                backend=default_backend()
+            )
+
+            out = encryptMsg(response, client_public_key)
+
+            await websocket.send(out)
 
 start_server = websockets.serve(sioRepository, 'localhost', 7654)
 
