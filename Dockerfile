8a938f6312db39df4a6c9ae4316ac5ef14bc0c54
joaoalegria <joao.p@ua.pt>
2019-01-25 00:50:34 +0000

    adicao Dockerfile e requirements

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.6
+RUN mkdir -p /app
+WORKDIR /app
+COPY . /app
+RUN pip install -r requirements.txt
+ENTRYPOINT python sioRepository.py
+#ENTRYPOINT python sioManager.py
--- sioManager.py
+++ sioManager.py
@@ -77,6 +77,7 @@ async def sioManager(websocket, path):
                 await websocket.send(out)
 
 start_server = websockets.serve(sioManager, 'localhost', 8765)
+#start_server = websockets.serve(sioManager, '0.0.0.0', 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
